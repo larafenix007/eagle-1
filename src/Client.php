0f3a463435ffa69e6201fb8b5286a07279ea2767
Alexey Nazarov <o4kan@ya.ru>
2017-06-30 19:58:32 +0300

    change Client and Eagle Facade for right use package

--- src/Client.php
+++ src/Client.php
@@ -2,12 +2,11 @@
 
 namespace Siqwell\Eagle;
 
+use Siqwell\Eagle\Api\AlbumApi;
+use Siqwell\Eagle\Api\RecordApi;
+use Siqwell\Eagle\Api\TranslationApi;
 use Siqwell\Eagle\HttpClient\HttpClient;
-use Siqwell\Eagle\Api\FilmApi;
 use Siqwell\Eagle\Api\FilterApi;
-use Siqwell\Eagle\Api\MetaApi;
-use Siqwell\Eagle\Api\NameApi;
-use Siqwell\Eagle\Api\SearchApi;
 
 /**
  * Class Client
@@ -23,7 +22,7 @@ class Client
      * @var \Siqwell\Eagle\ApiToken
      */
     protected $apiToken;
-    
+
     /**
      * Client constructor.
      * @param \Siqwell\Eagle\ApiToken $apiToken
@@ -33,12 +32,36 @@ class Client
     {
         $this->client = new HttpClient($apiToken, $options);
     }
-    
+
+    /**
+     * @return AlbumApi
+     */
+    public function Album() : AlbumApi
+    {
+        return new AlbumApi($this->client, new Methods());
+    }
+
     /**
      * @return FilterApi
      */
     public function Filter() : FilterApi
     {
         return new FilterApi($this->client, new Methods());
+    }
+
+    /**
+     * @return RecordApi
+     */
+    public function Record() : RecordApi
+    {
+        return new RecordApi($this->client, new Methods());
+    }
+
+    /**
+     * @return TranslationApi
+     */
+    public function Translation() : TranslationApi
+    {
+        return new TranslationApi($this->client, new Methods());
     }
 }
--- src/Facades/Eagle.php
+++ src/Facades/Eagle.php
@@ -4,6 +4,15 @@ namespace Siqwell\Eagle\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Eagle
+ * @package Siqwell\Eagle\Facades
+ *
+ * @method static \Siqwell\Eagle\Api\AlbumApi Album()
+ * @method static \Siqwell\Eagle\Api\FilterApi Filter()
+ * @method static \Siqwell\Eagle\Api\RecordApi Record()
+ * @method static \Siqwell\Eagle\Api\TranslationApi Translation()
+ */
 class Eagle extends Facade
 {
     /**
